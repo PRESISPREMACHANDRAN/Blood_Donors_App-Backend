9fbd0dda998d3793ba22f263445a0b3b35eec758
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-06 21:06:14 +0530

    add donor branch created

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.use(cors());
 
 
 
+//ADD DONOR
+app.post("/add",(req,res)=>{
+  var data=req.body
+res.json({"status":"success","data":data})
+})
+
 app.listen(4000, () => {
   console.log("server starts running.....");
 });
