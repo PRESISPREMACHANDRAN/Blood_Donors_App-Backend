0aff5c232aecccf205e0a3832ec335e3a0cc031c
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-06 21:19:02 +0530

    dbConnectivity branch created

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { donorModel } = require("./Model/DonorModel");
 
 const app = express();
 
@@ -8,13 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+mongoose.connect(
+  "mongodb+srv://presi123:presi123@cluster0.dfo33ti.mongodb.net/BloodDonorDB?retryWrites=true&w=majority"
+);
+
+
 
 
 //ADD DONOR
-app.post("/add",(req,res)=>{
+app.post("/add",async(req,res)=>{
   var data=req.body
-res.json({"status":"success","data":data})
+  let donor = new donorModel(data);
+  let result = await donor.save();
+res.json({"status":"success","data":result})
 })
+
+
+
 
 app.listen(4000, () => {
   console.log("server starts running.....");
