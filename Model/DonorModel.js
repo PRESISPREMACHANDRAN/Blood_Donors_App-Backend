9cb7ea4ef44bb59d90aadd5ee4359bd4cefbcd00
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-03-25 10:17:43 +0530

    changes

--- Model/DonorModel.js
+++ Model/DonorModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const donorSchema = mongoose.Schema({
-  firstName: String,
+  firstName: { type: String ,required:true},
   lastName: String,
   gender: String,
   email: String,
